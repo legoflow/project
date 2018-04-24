bb895737ddc8d846e90e54497a3f4f51b9bf1001
lijialiang <lijialiang@yy.com>
2018-04-24 14:10:28 +0800

    [update] use format YAML

--- index.js
+++ index.js
@@ -3,37 +3,9 @@
 const path = require('path');
 const fs = require('fs-extra');
 const YAML = require('yamljs');
-const lineReader = require('line-reader');
+const formatYamlFile = require('format-yaml');
 
 const getProjectType = require('./getProjectType');
-
-const formatYamlFile = ( file ) => {
-    let newFileContent = '';
-    let row = 0;
-
-    return new Promise( ( resolve, reject ) => {
-        lineReader.eachLine( file, { encoding: 'utf8' }, ( line, last ) => {
-            ++row;
-
-            if ( typeof line[ 0 ] !== 'undefined' && /\S/.test( line[ 0 ]) && line[ 0 ] != '' ) {
-                if ( newFileContent == '' ) {
-                    newFileContent = line;
-                }
-                else {
-                    newFileContent += '\n';
-                    newFileContent += `\n${ line }`;
-                }
-            }
-            else {
-                newFileContent += `\n${ line }`;
-            }
-
-            if ( last ) {
-                resolve( newFileContent );
-            }
-        } );
-    } );
-}
 
 module.exports = async ( data ) => {
     let { name, type, path: projectPath, version, isESNext, isSourcePath, author, c_version } = data;
