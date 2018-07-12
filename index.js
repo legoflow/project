fcfe963af281d6f75ae885dc50dc85b2291df65d
lijialiang <lijialiang@yy.com>
2018-07-12 14:42:15 +0800

    chore(release): v0.0.22

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 'use strict';
 
+const chalk = require('chalk');
+const globby = require('globby');
 const glob = require('glob');
 const path = require('path');
 const fs = require('fs-extra');
+const axios = require('axios');
 const legoflowConfig = require('legoflow-config');
 const YAML = require('yamljs');
 const shell = require('shelljs');
 const formatYamlFile = require('format-yaml');
 
+const replaceInfo = ( { name, version, c_version, author, isESNext, type }, str ) => {
+    return str
+                // 替换 项目名称
+                .replace( /\[name\]/g, name )
+                // 替换 项目版本号
+                .replace( /\[version\]/g, version )
+                // 替换 开发者用户
+                .replace( /\[author\]/g, author )
+                // 替换 lf 版本
+                .replace( /\[c_version\]/g, c_version )
+                // 替换 项目类型
+                .replace( /\[type\]/g, type )
+                // 替换 是否使用 ESNext
+                .replace( /\[ESNext\]/g, isESNext )
+}
+
+// 获取默认项目类型模板
 const getDefalutProjectType = ( ) => {
     const { project } = JSON.parse( fs.readFileSync( path.resolve( __dirname, './package.json' ), 'utf8' ) );
 
@@ -18,6 +38,7 @@ const getDefalutProjectType = ( ) => {
     return project;
 }
 
+// 获取本地自定义项目模板
 const getCustomProjectType = ( ) => {
     const customProjectPath = legoflowConfig.get( 'customProjectPath' ) || '';
 
@@ -34,12 +55,30 @@ const getCustomProjectType = ( ) => {
     return customProject;
 }
 
-const getProjectType = ( ) => {
-    return Object.assign( getDefalutProjectType( ), getCustomProjectType( ) );
+// 获取项目模板
+const getProjectType = async ( ) => {
+    const projectTypes = Object.assign( getDefalutProjectType( ), getCustomProjectType( ) );
+
+    if ( !legoflowConfig.get('loadNPMLegoFlowTemplate') ) {
+        return projectTypes;
+    }
+
+    const { data } = await axios('https://npm.taobao.org/browse/keyword/legoflow-template-?type=json');
+
+    const npmTemplate = [ ];
+
+    if ( data && data.packages ) {
+        data.packages.forEach( ( item ) => {
+            projectTypes[ item.name ] = `@npm@`;
+        } )
+    }
+
+    return projectTypes;
 }
 
 exports.getProjectType = getProjectType;
 
+// 新建默认类型项目
 const newDefaultProject = async ( data ) => {
     let { name, type, path: projectPath, version, isESNext, isSourcePath, author, c_version, description = '', isESLint, from } = data;
 
@@ -102,6 +141,8 @@ const newDefaultProject = async ( data ) => {
             "vue-router": "^3.0.1",
             "vuex": "^3.0.1"
         }
+
+        data.newProjectSuccessMessage = `➜ You can ${ chalk.blue.bold( `**cd ${ name }**` ) } and run ${ chalk.blue.bold( '**lf dev**' ) } to start dev.workflow`;
     }
 
     switch ( type ) {
@@ -185,6 +226,7 @@ const newDefaultProject = async ( data ) => {
     return data;
 }
 
+// 新建自定义路径模板
 const newCustomProject = async ( data ) => {
     let { name, type, path: projectPath, version, isESNext, isSourcePath, author, c_version } = data;
 
@@ -203,17 +245,8 @@ const newCustomProject = async ( data ) => {
         return '该项目类型缺少模板 package.json';
     }
 
-    const replaceInfo = ( str ) => {
-        return str.replace( /\[name\]/g, name )
-                    .replace( /\[version\]/g, version )
-                    .replace( /\[author\]/g, author )
-                    .replace( /\[c_version\]/g, c_version )
-                    .replace( /\[type\]/g, type )
-                    .replace( /\[ESNext\]/g, isESNext )
-    }
-
-    const yamlConfig = replaceInfo( fs.readFileSync( yamlConfigPath, 'utf8' ) );
-    const packageJson = replaceInfo( fs.readFileSync( packageJsonPath, 'utf8' ) );
+    const yamlConfig = replaceInfo( data, fs.readFileSync( yamlConfigPath, 'utf8' ) );
+    const packageJson = replaceInfo( data, fs.readFileSync( packageJsonPath, 'utf8' ) );
 
     fs.ensureDirSync( projectPath );
 
@@ -233,8 +266,68 @@ const newCustomProject = async ( data ) => {
     return data;
 }
 
+// 新建 npm 模板
+const newNpmProject = async ( data ) => {
+    let { name, type, path: projectPath, version, isESNext, isSourcePath, author, c_version } = data;
+
+    fs.mkdirsSync( projectPath );
+
+    shell.cd( projectPath );
+
+    const packageJsonPath = path.resolve( projectPath, 'package.json' );
+    const legoflowConfigPath = path.resolve( projectPath, 'legoflow.yml' );
+
+    fs.writeFileSync( packageJsonPath, `{"name":"${ name }"}` );
+
+    if ( shell.exec( `npm i ${ type } --save-dev` ).code !== 0 ) {
+        return 'install NPM template error';
+    }
+
+    console.log( '➜ install template success' );
+
+    // 重写配置
+    const templatePath = path.resolve( projectPath, `./node_modules/${ type }/template` );
+
+    const yamlConfig = replaceInfo( data, fs.readFileSync( path.resolve( templatePath, 'legoflow.yml' ), 'utf8' ) );
+    let packageJson = replaceInfo( data, fs.readFileSync( path.resolve( templatePath, 'package.json' ), 'utf8' ) );
+
+    fs.writeFileSync( path.resolve( projectPath, 'legoflow.yml' ), yamlConfig );
+
+    try {
+        packageJson = JSON.parse( packageJson );
+
+        packageJson.devDependencies = Object.assign( (JSON.parse( fs.readFileSync( packageJsonPath ) )).devDependencies, packageJson.devDependencies || { } );
+    } catch ( error ) {
+        print.error( error );
+    }
+
+    fs.writeFileSync( path.resolve( projectPath, 'package.json' ), JSON.stringify( packageJson, null, 2 ) );
+
+    console.log( '➜ rewrite package.json & legoflow.yml success' );
+
+    const otherFiles = await globby([ `${ templatePath }/**/*`, `!${ templatePath }/package.json`, `!${ templatePath }/legoflow.yml` ] );
+
+    for ( let f of otherFiles ) {
+        fs.copySync( f, path.resolve( projectPath, path.relative( templatePath, f ) ) );
+    }
+
+    console.log( '➜ copy other files success' );
+
+    if ( shell.exec( `npm i` ).code !== 0 ) {
+        return 'install dependencies error';
+    }
+
+    console.log( '➜ install dependencies success' );
+
+    data.newProjectSuccessMessage = `➜ You can ${ chalk.blue.bold( `**cd ${ name }**` ) } and run ${ chalk.blue.bold( '**lf dev**' ) } to start dev.workflow`;
+
+    return data;
+}
+
+
+// 新建项目
 exports.new = async ( data ) => {
-    let { name, path: projectPath, isSourcePath, type } = data;
+    let { name, path: projectPath, isSourcePath, type, typeSourcePath } = data;
 
     if ( !isSourcePath ) {
         data.path = projectPath = path.resolve( projectPath, `./${ name }` );
@@ -248,7 +341,10 @@ exports.new = async ( data ) => {
         return '路径存在其他文件';
     }
 
-    if ( getCustomProjectType( )[ type ] ) {
+    if ( typeSourcePath === '@npm@' ) {
+        return await newNpmProject( data );
+    }
+    else if ( getCustomProjectType( )[ type ] ) {
         return await newCustomProject( data );
     }
     else if ( getDefalutProjectType( )[ type ] ) {
